880d45b414f4a5c8acf4e7e2e548a8e9cc5b8073
Georges Savoundararadj <savoundg@gmail.com>
2017-11-23 12:23:31 -0800

    deploy.sh: install vim plugin dein

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,11 @@ stow tmux
 stow Xresources
 
 xrdb ~/.Xresources
+
+# install dein
+if [ ! -d ~/.config/nvim/bundles ]; then
+	curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > vim/installer.sh
+	mkdir ~/.config/nvim/bundles/
+	sh ./vim/installer.sh ~/.config/nvim/bundles
+	rm vim/installer.sh
+fi
