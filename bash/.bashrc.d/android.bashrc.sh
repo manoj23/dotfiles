3618869e075694c98d0788d53a543be2c983f0ad
Georges Savoundararadj <savoundg@gmail.com>
2018-07-04 15:37:28 -0700

    bash: android: add android_mk_raw

--- bash/.bashrc.d/android.bashrc.sh
+++ bash/.bashrc.d/android.bashrc.sh
@@ -11,3 +11,28 @@ android_flash_dtb()
 		sudo abootimg -u "/dev/$1" -s "arch/arm/boot/dts/$2";
 	fi
 }
+
+android_mk_raw()
+{
+	if [ $# -lt 1 ]; then
+		echo "Not enough arguments, Bye!"
+		return 1
+	fi
+
+	if [ ! -e "$1" ]; then
+		echo "$1 does not exist, Bye!"
+		return 1
+	fi
+
+	local img_file="$1"
+	local img_file_extension=${img_file##*.}
+
+	if [ "$img_file_extension" != "img" ]; then
+		echo "$img_file does not end with .img, Bye!"
+		return 1
+	fi
+
+	local raw_file="$(basename "$img_file").raw"
+
+	simg2img "$img_file" "$raw_file"
+}
