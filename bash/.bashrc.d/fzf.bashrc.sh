03630327ec682adc017c1e8619ad418d7c592df0
Georges Savoundararadj <savoundg@gmail.com>
2022-06-09 10:31:16 -0700

    bash: fzf: add C-b to list branches

--- bash/.bashrc.d/fzf.bashrc.sh
+++ bash/.bashrc.d/fzf.bashrc.sh
@@ -1,5 +1,8 @@
 # Copied from https://github.com/junegunn/fzf/wiki/Examples
 
+bind -x '"\C-p": ff'
+bind -x '"\C-b": fco'
+
 # ff [FUZZY PATTERN] - Open the selected file with the default editor
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
@@ -8,8 +11,6 @@ ff() {
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 }
-
-bind -x '"\C-p": ff'
 
 # fd - cd to selected directory
 fd() {
