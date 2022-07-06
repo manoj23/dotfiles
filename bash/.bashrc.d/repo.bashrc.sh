68e0ae4a4c580c77584ce446733da9b012a0ba15
Georges Savoundararadj <savoundg@gmail.com>
2022-07-05 18:47:02 -0700

    bash: repo: add repo_install

--- bash/.bashrc.d/repo.bashrc.sh
+++ bash/.bashrc.d/repo.bashrc.sh
@@ -8,6 +8,13 @@ repo()
 	fi
 }
 
+repo_install()
+{
+	mkdir -p ~/.bin
+	curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
+	chmod a+rx ~/.bin/repo
+}
+
 repo_sync()
 {
 	repo sync -j16 -qc
