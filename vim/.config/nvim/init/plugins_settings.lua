e0d954d4e89e89da0ea74d73ea55c130390cfe00
Georges Savoundararadj <savoundg@gmail.com>
2024-08-04 08:47:58 +0200

    vim: treesitter: disable for large file

--- vim/.config/nvim/init/plugins_settings.lua
+++ vim/.config/nvim/init/plugins_settings.lua
@@ -12,3 +12,22 @@ for _, lsp in ipairs(servers) do
     -- on_attach = my_custom_on_attach,
   }))
 end
+
+vim.opt.runtimepath:append(',~/.cache/dein/repos/github.com/nvim-treesitter/nvim-treesitter/')
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "rust" },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    additional_vim_regex_highlighting = false,
+  },
+}
