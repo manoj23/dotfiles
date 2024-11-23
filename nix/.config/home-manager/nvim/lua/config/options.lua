3ccd27d0b17bb751ec5aabeea49b5ee49db40510
Georges Savoundararadj <savoundg@gmail.com>
2024-11-23 20:40:35 +0100

    home-manager: nvim: add SetTab/UseTab/UseSpace4

--- nix/.config/home-manager/nvim/lua/config/options.lua
+++ nix/.config/home-manager/nvim/lua/config/options.lua
@@ -3,3 +3,36 @@
 -- Add any additional options here
 vim.g.lazyvim_picker = "fzf"
 vim.g.autoformat = false
+vim.opt.smarttab = false
+vim.opt.softtabstop = 0
+
+local SetTab = function(args)
+  local tabsize = tonumber(args.args) or 4
+  vim.opt.tabstop = tabsize
+  vim.opt.shiftwidth = tabsize
+end
+local UseSpace = function(args)
+  local tabsize = tonumber(args.args) or 4
+  SetTab({ args = tabsize })
+  vim.opt.expandtab = true
+end
+
+local UseTab = function(args)
+  local tabsize = tonumber(args.args) or 4
+  SetTab({ args = tabsize })
+  vim.opt.expandtab = false  -- Use tabs instead of spaces
+end
+
+vim.api.nvim_create_user_command(
+  "UseTab", UseTab, { nargs = "?" }
+)
+
+vim.api.nvim_create_user_command(
+  "UseSpace", UseSpace, { nargs = "?" }
+)
+
+vim.api.nvim_create_user_command(
+  "SetTab", SetTab, { nargs = "?" }
+)
+
+UseSpace({ args = "4" })
