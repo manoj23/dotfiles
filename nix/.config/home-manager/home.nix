511742762be8251cdf7f20ac27941c492b8940eb
Georges Savoundararadj <savoundg@gmail.com>
2025-08-05 01:00:33 +0200

    nix: home-manager: bump 24.05 -> 25.05

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -19,7 +19,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.05";
+  home.stateVersion = "25.05";
 
   nix = {
     package = pkgs.nix;
