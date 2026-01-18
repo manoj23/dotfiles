9aa72064a86da979669757b75e8c0e97e7cee558
Georges Savoundararadj <savoundg@gmail.com>
2026-01-18 10:48:59 +0000

    bash: git: improve cherry_pick

--- bash/.bashrc.d/git.bashrc.sh
+++ bash/.bashrc.d/git.bashrc.sh
@@ -2,5 +2,11 @@ cherry_pick()
 {
 	if [ -n "$1" ]; then
 		git --git-dir="$CHERRY_PICK_DIR/.git" format-patch -k -1 --stdout "$1" | git am -3 -k;
+		if [ $? ]; then
+			git commit --amend -m "$(git log --format=%B -n 1)
+(cherry picked from commit $1)"
+		else
+			echo "Copy/paste (cherry picked from commit $1)"
+		fi
 	fi
 }
